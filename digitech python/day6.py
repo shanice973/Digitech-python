1cabb0d7093ae663fa452c274159b324d53af574
shanice973 <nyabokeshanice934@gmail.com>
2025-10-09 12:10:28 +0300

    Add files via upload

---
+++ digitech python/day6.py
@@ -0,0 +1,22 @@
+a = 10
+b = 8
+
+calc= a^b
+binary = bin(calc)
+
+print(f"{calc}and it's binary is {binary}")
+
+a =8
+b =6
+
+calc = a & b
+binary = bin(calc)
+
+print(f"{calc} and it's binary is {binary}")
+
+a=5
+b = 4
+calc = a&b
+binary = bin(calc)
+
+print(f"{calc} and it's binary is {binary}")
